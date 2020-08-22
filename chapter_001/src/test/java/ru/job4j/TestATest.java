90511a352901ad111ee1a512f60327a2d1b7c66a
BBergsJ <goldlike888@gmail.com>
2020-08-22 23:25:47 +0300

    Instruments

---
+++ chapter_001/src/test/java/ru/job4j/TestATest.java
@@ -0,0 +1,10 @@
+package ru.job4j;
+
+import junit.framework.TestCase;
+
+public class TestATest extends TestCase {
+
+    public void testMain() {
+        assertTrue(true);
+    }
+}
---
+++ src/main/java/ru/job4j/TestTest.java
@@ -0,0 +1,7 @@
+package ru.job4j;
+
+public class TestTest {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
