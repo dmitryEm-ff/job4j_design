6f3b14aab686f1a4da89352bd06dc66c34cf95d5
BBergsJ <goldlike888@gmail.com>
2020-08-23 14:27:01 +0300

    Instruments

--- chapter_001/src/main/java/ru/job4j/TestA.java
+++ chapter_001/src/main/java/ru/job4j/TestA.java
@@ -3,6 +3,5 @@ package ru.job4j;
 public class TestA {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hello World!");
     }
 }
