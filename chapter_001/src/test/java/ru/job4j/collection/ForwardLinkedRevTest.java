7e0fafda261a0ed6c911341e0f4be1d476655024
BBergsJ <goldlike888@gmail.com>
2020-09-17 21:04:40 +0300

    Перевернуть связанный список

---
+++ chapter_001/src/test/java/ru/job4j/collection/ForwardLinkedRevTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.collection;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.Iterator;
+
+public class ForwardLinkedRevTest {
+
+    @Test
+    public void whenAddThenIter() {
+        ForwardLinkedRev<Integer> linked = new ForwardLinkedRev<>();
+        linked.add(1);
+        linked.add(2);
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(1));
+        assertThat(it.next(), is(2));
+    }
+
+    @Test
+    public void whenAddAndRevertThenIter() {
+        ForwardLinkedRev<Integer> linked = new ForwardLinkedRev<>();
+        linked.add(1);
+        linked.add(2);
+        linked.revert();
+        Iterator<Integer> it = linked.iterator();
+        assertThat(it.next(), is(2));
+        assertThat(it.next(), is(1));
+    }
+}
