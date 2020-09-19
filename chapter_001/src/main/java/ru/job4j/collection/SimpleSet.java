8e0d854050eb645241218cdc522d52e571e26cb3
BBergsJ <goldlike888@gmail.com>
2020-09-19 22:16:12 +0300

    Реализовать коллекцию Set на массиве

--- chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleSet.java
@@ -2,48 +2,20 @@ package ru.job4j.collection;
 
 import java.util.*;
 
-public class SimpleSet<T> implements Iterable<T> {
+public class SimpleSet<E> implements Iterable<E> {
 
-    private int modCount;
-    private int elemCount;
-    private Object[] container = new Object[10];
+    private SimpleArray<E> array = new SimpleArray<>();
 
-    public T get(int index) {
-        Objects.checkIndex(index, elemCount);
-        return (T) container[index];
+    public E get(int index) {
+        return array.get(index);
     }
 
-    public void add(T model) {
-        if (elemCount == container.length - 1) {
-            container = Arrays.copyOf(container, (container.length * 3) / 2 + 1);
-        }
-        container[elemCount] = model;
-        modCount++;
-        elemCount++;
+    public void add(E model) {
+        array.add(model);
     }
 
     @Override
-    public Iterator<T> iterator() {
-        return new Iterator<T>() {
-
-            private int expectedModCount  = modCount;
-            int currentIndex = 0;
-
-            @Override
-            public boolean hasNext() {
-                if (expectedModCount != modCount) {
-                    throw new ConcurrentModificationException();
-                }
-                return currentIndex < elemCount;
-            }
-
-            @Override
-            public T next() {
-                if (!hasNext()) {
-                    throw new NoSuchElementException();
-                }
-                return (T) container[currentIndex++];
-            }
-        };
+    public Iterator<E> iterator() {
+        return array.iterator();
     }
 }
