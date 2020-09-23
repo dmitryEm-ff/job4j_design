881a37057d91ad33d0fcc7d970c9bf60db269451
BBergsJ <goldlike888@gmail.com>
2020-09-23 23:44:12 +0300

    Реализовать собственную структуру данных - HashMap

--- chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -4,6 +4,17 @@ import java.util.Iterator;
 
 public class SimpleHashMap<K, V> implements Iterable {
 
+    public boolean insert(K key, V value) {
+
+    }
+
+    public V get(K key) {
+
+    }
+
+    public boolean delete(K key) {
+
+    }
 
     @Override
     public Iterator iterator() {
