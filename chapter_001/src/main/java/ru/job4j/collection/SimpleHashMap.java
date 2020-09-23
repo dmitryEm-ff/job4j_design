fd13f422ce79f83df8132df910c0b08becf7e096
BBergsJ <goldlike888@gmail.com>
2020-09-23 23:35:25 +0300

    Без переопределения equals и hashCode

---
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -0,0 +1,11 @@
+package ru.job4j.collection;
+
+import java.util.Iterator;
+
+public class SimpleHashMap<K, V> implements Iterable {
+
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+}
