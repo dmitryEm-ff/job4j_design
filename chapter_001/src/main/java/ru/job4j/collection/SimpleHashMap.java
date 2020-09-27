b73d1ebaf487beb236ebf3cb5ffc0bf044afb6a8
BBergsJ <goldlike888@gmail.com>
2020-09-27 20:11:15 +0300

    Реализовать собственную структуру данных - HashMap

--- chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
+++ chapter_001/src/main/java/ru/job4j/collection/SimpleHashMap.java
@@ -4,6 +4,13 @@ import java.util.Iterator;
 
 public class SimpleHashMap<K, V> implements Iterable {
 
+    private Entry<K, V>[] table;
+    private final float loadFactor = 0.75f;
+    private final float capacity = 16f;
+    private int threshold;
+    private int size;
+    private int modCount;
+
     public boolean insert(K key, V value) {
 
     }
@@ -19,5 +26,25 @@ public class SimpleHashMap<K, V> implements Iterable {
     @Override
     public Iterator iterator() {
         return null;
+    }
+
+    public static class Entry<K, V> {
+        private K key;
+        private V value;
+        private int hash;
+
+        public Entry(K key, V value, int hash) {
+            this.key = key;
+            this.value = value;
+            this.hash = hash;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
     }
 }
