6cdd30d1521dd8c1d1e6392fdd12bd117252c3c2
BBergsJ <goldlike888@gmail.com>
2020-10-03 21:00:56 +0300

    Создать элементарную структуру дерева

--- chapter_001/src/main/java/ru/job4j/tree/Tree.java
+++ chapter_001/src/main/java/ru/job4j/tree/Tree.java
@@ -1,27 +1,13 @@
 package ru.job4j.tree;
 
 import java.util.*;
+import java.util.function.Predicate;
 
 class Tree<E> implements SimpleTree<E> {
     private final Node<E> root;
 
     Tree(final E root) {
         this.root = new Node<>(root);
-    }
-
-    public boolean isBinary() {
-        boolean rsl = true;
-        Queue<Node<E>> data = new LinkedList<>();
-        data.offer(this.root);
-        while (!data.isEmpty()) {
-            Node<E> el = data.poll();
-            if (el.children.size() > 3) {
-                rsl = false;
-                break;
-            }
-            data.addAll(el.children);
-        }
-        return rsl;
     }
 
     @Override
@@ -37,19 +23,70 @@ class Tree<E> implements SimpleTree<E> {
         return false;
     }
 
+//    @Override
+//    public Optional<Node<E>> findBy(E value) {
+//        Optional<Node<E>> rsl = Optional.empty();
+//        Queue<Node<E>> data = new LinkedList<>();
+//        data.offer(this.root);
+//        while (!data.isEmpty()) {
+//            Node<E> el = data.poll();
+//            if (el.value.equals(value)) {
+//                rsl = Optional.of(el);
+//                break;
+//            }
+//            data.addAll(el.children);
+//        }
+//        return rsl;
+//    }
+//
+//    public boolean isBinary() {
+//        boolean rsl = true;
+//        Queue<Node<E>> data = new LinkedList<>();
+//        data.offer(this.root);
+//        while (!data.isEmpty()) {
+//            Node<E> el = data.poll();
+//            if (el.children.size() > 3) {
+//                rsl = false;
+//                break;
+//            }
+//            data.addAll(el.children);
+//        }
+//        return rsl;
+//    }
+
     @Override
     public Optional<Node<E>> findBy(E value) {
-        Optional<Node<E>> rsl = Optional.empty();
+        Predicate<Node<E>> predicate =  new Predicate<Node<E>>() {
+            @Override
+            public boolean test(Node<E> eNode) {
+                return eNode.value.equals(value);
+            }
+        };
+        return (Optional<Node<E>>) commonCheck(predicate);
+    }
+
+    public boolean isBinary() {
+        Predicate<Node<E>> predicate = new Predicate<Node<E>>() {
+            @Override
+            public boolean test(Node<E> eNode) {
+                return eNode.children.size() > 3;
+            }
+        };
+        return (boolean) commonCheck(predicate);
+    }
+
+    public E commonCheck(Predicate<Node<E>> predicate) {
+        Optional<E> rsl = Optional.empty();
         Queue<Node<E>> data = new LinkedList<>();
         data.offer(this.root);
         while (!data.isEmpty()) {
             Node<E> el = data.poll();
-            if (el.value.equals(value)) {
-                rsl = Optional.of(el);
+            if (predicate.test(el)) {
+                rsl = (Optional<E>) Optional.of(el);
                 break;
             }
             data.addAll(el.children);
         }
-        return rsl;
+        return (E) rsl;
     }
 }
