2fd810575ba27530328a015b519baf3d339b8cc1
BBergsJ <goldlike888@gmail.com>
2020-10-13 22:22:15 +0300

    0. Видео[4952#346764]

---
+++ chapter_001/src/main/java/ru/job4j/statistic/Analize.java
@@ -0,0 +1,86 @@
+package ru.job4j.statistic;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Analize {
+
+    public Info diff(List<User> previous, List<User> current) {
+        return null;
+    }
+
+    public static class User {
+        int id;
+        String name;
+
+        public User(int id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            User user = (User) o;
+            return id == user.id
+                    && Objects.equals(name, user.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(id, name);
+        }
+    }
+
+    public static class Info {
+        int added;
+        int changed;
+        int deleted;
+
+        public Info(int added, int changed, int deleted) {
+            this.added = added;
+            this.changed = changed;
+            this.deleted = deleted;
+        }
+
+        public int getAdded() {
+            return added;
+        }
+
+        public int getChanged() {
+            return changed;
+        }
+
+        public int getDeleted() {
+            return deleted;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) {
+                return true;
+            }
+            if (o == null || getClass() != o.getClass()) {
+                return false;
+            }
+            Info info = (Info) o;
+            return added == info.added
+                    && changed == info.changed
+                    && deleted == info.deleted;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(added, changed, deleted);
+        }
+    }
+}
