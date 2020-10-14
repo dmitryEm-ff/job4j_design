9b7ccf71264ddabad0f4c8bbf32e02037d220a3f
BBergsJ <goldlike888@gmail.com>
2020-10-14 23:49:41 +0300

    Статистику по коллекции.

--- chapter_001/src/main/java/ru/job4j/statistic/Analize.java
+++ chapter_001/src/main/java/ru/job4j/statistic/Analize.java
@@ -7,7 +7,19 @@ import java.util.Objects;
 public class Analize {
 
     public Info diff(List<User> previous, List<User> current) {
-        Info info = new Info();
+        Info info = new Info(0, 0, 0);
+        for (User userPrev : previous) {
+            for (User userCur : current) {
+                if (userCur.id != userPrev.id) {
+                    info.added++;
+                    break;
+                }
+                if (userCur.name != userPrev.name) {
+                    info.changed++;
+                }
+            }
+        }
+        info.deleted = (previous.size() + info.added) - current.size();
         return info;
     }
 
