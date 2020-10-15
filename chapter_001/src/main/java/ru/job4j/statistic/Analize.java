689096e793d97dcdf7e0051ae0715221d4deb9c3
BBergsJ <goldlike888@gmail.com>
2020-10-15 22:57:45 +0300

    Статистику по коллекции.

--- chapter_001/src/main/java/ru/job4j/statistic/Analize.java
+++ chapter_001/src/main/java/ru/job4j/statistic/Analize.java
@@ -1,7 +1,6 @@
 package ru.job4j.statistic;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Objects;
 
 public class Analize {
@@ -39,24 +38,6 @@ public class Analize {
         public String getName() {
             return name;
         }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            User user = (User) o;
-            return id == user.id
-                    && Objects.equals(name, user.name);
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(id, name);
-        }
     }
 
     public static class Info {
@@ -83,25 +64,6 @@ public class Analize {
 
         public int getDeleted() {
             return deleted;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) {
-                return true;
-            }
-            if (o == null || getClass() != o.getClass()) {
-                return false;
-            }
-            Info info = (Info) o;
-            return added == info.added
-                    && changed == info.changed
-                    && deleted == info.deleted;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(added, changed, deleted);
         }
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/statistic/AnalizeTest.java
@@ -0,0 +1,58 @@
+package ru.job4j.statistic;
+
+import org.junit.Assert;
+import org.junit.Test;
+import static ru.job4j.statistic.Analize.Info;
+import static ru.job4j.statistic.Analize.User;
+
+import  static org.hamcrest.core.Is.is;
+import  static org.junit.Assert.assertThat;
+import java.util.*;
+
+public class AnalizeTest {
+
+    @Test
+    public void whenItemsAdded() {
+        List<User> previous = List.of(new User(1, "One"));
+        List<User> current = List.of(new User(1, "One"), new User(2, "Two"));
+        Info info = new Analize().diff(previous, current);
+        Info expected = new Info(1, 0, 0);
+        assertThat(expected, is(info));
+    }
+
+    @Test
+    public void whenItemsChanged() {
+        List<User> previous = List.of(new User(1, "One"), new User(2, "Two"));
+        List<User> current = List.of(new User(1, "One"), new User(3, "Three"));
+        Info info = new Analize().diff(previous, current);
+        Info expected = new Info(1, 0, 1);
+        assertThat(expected, is(info));
+    }
+
+    @Test
+    public void whenItemsRemoved() {
+        List<User> previous = List.of(new User(1, "One"), new User(2, "Two"));
+        List<User> current = List.of(new User(1, "One"));
+        Info info = new Analize().diff(previous, current);
+        Info expected = new Info(0, 0, 1);
+        assertThat(expected, is(info));
+    }
+
+    @Test
+    public void whenItemsNotChanged() {
+        List<User> previous = List.of(new User(1, "One"), new User(2, "Two"));
+        List<User> current = List.of(new User(1, "One"), new User(2, "Two"));
+        Info info = new Analize().diff(previous, current);
+        Info expected = new Info(0, 0, 0);
+        assertThat(expected, is(info));
+    }
+
+    @Test
+    public void whenItemsChangedName() {
+        List<User> previous = List.of(new User(1, "One"), new User(2, "Two"));
+        List<User> current = List.of(new User(1, "One"), new User(2, "Three"));
+        Info info = new Analize().diff(previous, current);
+        Info expected = new Info(0, 1, 0);
+        assertThat(expected, is(info));
+    }
+}
