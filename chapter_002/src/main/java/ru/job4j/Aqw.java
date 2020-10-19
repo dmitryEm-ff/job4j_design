f512627d35ed61b78694e5a60fce6b0177b1465a
BBergsJ <goldlike888@gmail.com>
2020-10-19 23:13:00 +0300

    FileOutputStream.

--- chapter_002/src/main/java/ru/job4j/Aqw.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j;
-
-public class Aqw {
-    public static void main(String[] args) {
-        System.out.println("1212");
-    }
-}
---
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -0,0 +1,20 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class ResultFile {
+    public static void main(String[] args) {
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int i = 1; i < 10; i++) {
+                for (int j = 1; j < 10; j++) {
+                    String s = j + " * " + i + " = " + (j * i) + "\n";
+                    System.out.println(s);
+                    out.write(s.getBytes());
+                }
+                System.out.println();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
