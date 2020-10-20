2088012f5b29ec1ee97aeed8d1de600a8885fd6d
BBergsJ <goldlike888@gmail.com>
2020-10-20 23:04:22 +0300

    FileOutputStream.

--- chapter_002/src/main/java/ru/job4j/io/ResultFile.java
+++ chapter_002/src/main/java/ru/job4j/io/ResultFile.java
@@ -7,7 +7,7 @@ public class ResultFile {
         try (FileOutputStream out = new FileOutputStream("result.txt")) {
             for (int i = 1; i < 10; i++) {
                 for (int j = 1; j < 10; j++) {
-                    String s = j + " * " + i + " = " + (j * i) + "\n";
+                    String s = j + " * " + i + " = " + (j * i) + System.lineSeparator();
                     System.out.println(s);
                     out.write(s.getBytes());
                 }
