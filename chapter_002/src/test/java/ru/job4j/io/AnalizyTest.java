c0f6d8f937b3e86355796372df7cf898657dcee3
BBergsJ <goldlike888@gmail.com>
2020-10-28 21:44:58 +0300

    Читаем файл конфигурации

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -6,6 +6,7 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.PrintWriter;
+import java.net.URL;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -16,6 +17,6 @@ public class AnalizyTest {
     @Test
     public void whenWriteFromServerLog() {
         File source = Analizy.class.getResource("server.log");
-
+        File target = Analizy.class.getResource("unavailable.log");
     }
 }
