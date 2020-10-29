62976f141a3cdd281475b3423fc8bded8b9ed63b
BBergsJ <goldlike888@gmail.com>
2020-10-29 23:17:17 +0300

    Читаем файл конфигурации

--- chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
+++ chapter_002/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -1,10 +1,13 @@
 package ru.job4j.io;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
@@ -14,7 +17,7 @@ import static org.junit.Assert.assertThat;
 public class AnalizyTest {
 
     @Test
-    public void whenWriteFromServerLog() throws IOException {
+    public void whenWriteFromServerLogFirstEdition() throws IOException {
         File source = new File("./src/main/resources/server.log");
         File target = new File("./src/main/resources/unavailable.log");
 
@@ -31,6 +34,32 @@ public class AnalizyTest {
         analizy.unavailable(source.getPath(), target.getPath());
         List<String> expected = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
         List<String> result = Files.readAllLines(target.toPath());
+        assertThat(result, is(expected));
+    }
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
+
+    @Test
+    public void whenWriteFromServerLogSecondEdition() throws IOException {
+        File source = folder.newFile("server.log");
+        File target = folder.newFile("unavailable.log");
+        List<String> array = List.of(
+                "200 10:56:01",
+                "500 10:57:01",
+                "400 10:58:01",
+                "200 10:59:01",
+                "500 11:01:02",
+                "200 11:02:02");
+        try (BufferedWriter out = new BufferedWriter(new FileWriter(source))) {
+            for (String s : array) {
+                out.write(s + System.lineSeparator());
+            }
+        }
+        Analizy analizy = new Analizy();
+        analizy.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+        List<String> result = Files.readAllLines(target.toPath());
+        List<String> expected = List.of("10:57:01;10:59:01", "11:01:02;11:02:02");
         assertThat(result, is(expected));
     }
 }
