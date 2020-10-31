0b3d80872bb8f77f632ae8119d4903fdb66d566e
BBergsJ <goldlike888@gmail.com>
2020-10-31 17:02:12 +0300

    Читаем файл конфигурации

--- chapter_002/src/main/java/ru/job4j/io/Analizy.java
+++ chapter_002/src/main/java/ru/job4j/io/Analizy.java
@@ -1,24 +1,30 @@
 package ru.job4j.io;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Analizy {
-    public void unavailable(String source, String target) {
-        try (BufferedReader reader = new BufferedReader(new FileReader(source));
-            BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+    public void unavailable(String source, String target) throws IOException {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader reader = new BufferedReader(new FileReader(source))) {
             String point = null;
             while (reader.ready()) {
                 String line = reader.readLine();
                 if ((line.contains("400") || line.contains("500")) && point == null) {
                     point = "error";
-                    writer.write(line.split(" ")[1] + ";");
+                    list.add(line.split(" ")[1] + ";");
                 } else if (point != null && !line.isEmpty() && !line.contains("400") && !line.contains("500")) {
-                    writer.write(line.split(" ")[1] + System.lineSeparator());
+                    list.add(line.split(" ")[1] + System.lineSeparator());
                     point = null;
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(target))) {
+            for (String s : list) {
+                writer.write(s);
+            }
         }
     }
 
