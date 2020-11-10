e43f8eb046fc465247dbdd5ddf5b4c60e3ff26a1
BBergsJ <goldlike888@gmail.com>
2020-11-11 00:12:27 +0300

    Сканирование файловой системы.[106929#346786]

--- chapter_002/src/test/java/ru/job4j/io/CleanerTest.java
+++ chapter_002/src/test/java/ru/job4j/io/CleanerTest.java
@@ -4,9 +4,13 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.*;
 import static org.hamcrest.Matchers.is;
@@ -16,12 +20,14 @@ public class CleanerTest {
 
     @Test
     public void whenDuplicateExist() throws IOException {
-        File file1 = new File("./src/main/resources/test.txt");
-        File file2 = new File("./src/main/test.txt");
+
+        Path file1 = Paths.get("./src/main/resources/test.txt");
+        Files.write(file1, List.of("Some text"));
+        Files.copy(file1, Paths.get("./src/main/java/ru/job4j/test.txt"), StandardCopyOption.REPLACE_EXISTING);
         Path start = Paths.get("./src/main");
 
         List<Path> rsl = Cleaner.search(start);
-        List<Path> expected = List.of(Paths.get("test.txt"));
-        assertThat(rsl, is(expected));
+        List<Path> expected = List.of(Paths.get("./src/main/resources/test.txt").toAbsolutePath());
+        assertThat(rsl.stream().map(Path::toAbsolutePath).collect(Collectors.toList()), is(expected));
     }
 }
