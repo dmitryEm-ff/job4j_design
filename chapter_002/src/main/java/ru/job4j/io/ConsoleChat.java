01ed71f1c3205a323dba501d32cbacd1e114b153
BBergsJ <goldlike888@gmail.com>
2020-11-19 23:55:03 +0300

    Кодировка

---
+++ chapter_002/src/main/java/ru/job4j/io/ConsoleChat.java
@@ -0,0 +1,23 @@
+package ru.job4j.io;
+
+public class ConsoleChat {
+    private final String path;
+    private final String botAnswers;
+    private static final String OUT = "закончить";
+    private static final String STOP = "стоп";
+    private static final String CONTINUE = "продолжить";
+
+    public ConsoleChat(String path, String botAnswers) {
+        this.path = path;
+        this.botAnswers = botAnswers;
+    }
+
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        ConsoleChat cc = new ConsoleChat("", "");
+        cc.run();
+    }
+}
