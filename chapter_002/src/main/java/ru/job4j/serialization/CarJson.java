0ca4f145581f4ba645793c3748fe564d257dad1a
BBergsJ <goldlike888@gmail.com>
2020-12-04 00:07:41 +0300

    Преобразование JSON в POJO. JsonObject

--- chapter_002/src/main/java/ru/job4j/serialization/CarJson.java
+++ chapter_002/src/main/java/ru/job4j/serialization/CarJson.java
@@ -2,6 +2,9 @@ package ru.job4j.serialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import netscape.javascript.JSObject;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.Arrays;
 
@@ -18,6 +21,22 @@ public class CarJson {
         this.carOwner = carOwner;
     }
 
+    public boolean isGas() {
+        return gas;
+    }
+
+    public int getYearOfManufacture() {
+        return yearOfManufacture;
+    }
+
+    public CarParam getCarParam() {
+        return carParam;
+    }
+
+    public String[] getCarOwner() {
+        return carOwner;
+    }
+
     @Override
     public String toString() {
         return "CarJson{" + "gas=" + gas + ", yearOfManufacture=" + yearOfManufacture
@@ -28,22 +47,34 @@ public class CarJson {
         CarJson carJson = new CarJson(true, 2012,
                 new CarParam("Lada", "white"), "Igor", "Denis", "Vitaly");
 
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(carJson));
+        JSONArray jsonArray = new JSONArray(carJson.getCarOwner());
+        JSONObject carBrandNColor = new JSONObject("{\"brand\":\"Lada\"}, {\"color\":\"white\"}");
 
-        final String carJsonMod = "{"
-                + "\"gas\":false,"
-                + "\"yearOfManufacture\":2016,"
-                + "\"carParam\":"
-                + "{"
-                + "\"brand\":\"Niva\","
-                + "\"color\":\"black\""
-                + "},"
-                + "\"carOwner\":"
-                + "[\"Vanya\",\"Olya\",\"Masha\"]"
-                + "}";
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("gas", carJson.isGas());
+        jsonObject.put("yearOfManufacture", carJson.getYearOfManufacture());
+        jsonObject.put("carParam", carBrandNColor);
+        jsonObject.put("carOwner", jsonArray);
 
-        final CarJson carJsonChanged = gson.fromJson(carJsonMod, CarJson.class);
-        System.out.println(carJsonChanged);
+        System.out.println(jsonObject.toString());
+
+        System.out.println(new JSONObject(carJson).toString());
+//        final Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(carJson));
+//
+//        final String carJsonMod = "{"
+//                + "\"gas\":false,"
+//                + "\"yearOfManufacture\":2016,"
+//                + "\"carParam\":"
+//                + "{"
+//                + "\"brand\":\"Niva\","
+//                + "\"color\":\"black\""
+//                + "},"
+//                + "\"carOwner\":"
+//                + "[\"Vanya\",\"Olya\",\"Masha\"]"
+//                + "}";
+//
+//        final CarJson carJsonChanged = gson.fromJson(carJsonMod, CarJson.class);
+//        System.out.println(carJsonChanged);
     }
 }
