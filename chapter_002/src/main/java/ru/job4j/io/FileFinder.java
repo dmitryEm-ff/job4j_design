643b52a326b5891ed407a439178b846f9cb70bc6
BBergsJ <goldlike888@gmail.com>
2020-12-22 22:11:49 +0300

    Поиск файлов по критерию

---
+++ chapter_002/src/main/java/ru/job4j/io/FileFinder.java
@@ -0,0 +1,9 @@
+package ru.job4j.io;
+
+import java.nio.file.Files;
+
+public class FileFinder {
+    public static void main(String[] args) {
+        FileFinderArgs fileFinderArgs = new FileFinderArgs(args);
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/io/FileFinderArgs.java
@@ -0,0 +1,9 @@
+package ru.job4j.io;
+
+public class FileFinderArgs {
+    private static String[] args;
+
+    public FileFinderArgs(String[] args) {
+        this.args = args;
+    }
+}
