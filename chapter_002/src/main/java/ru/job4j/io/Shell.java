2202c22a43c141cd6496463600884104b61b8a25
BBergsJ <goldlike888@gmail.com>
2020-12-25 00:48:27 +0300

    Поиск файлов по критерию

--- chapter_002/src/main/java/ru/job4j/io/Shell.java
+++ chapter_002/src/main/java/ru/job4j/io/Shell.java
@@ -1,17 +1,41 @@
 package ru.job4j.io;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class Shell {
 
-    private ArrayList pathDir = new ArrayList();
+    private List<String> pathDir = new ArrayList();
 
     public void cd(String path) {
-        if (path.endsWith("..") || path == "/") {
+//        if (path.endsWith("..")) {
+//            if (pathDir.size() < 1) {
+//                pathDir.add("/");
+//            } else if (pathDir.size() == 1) {
+//                pathDir.remove(0);
+//                pathDir.add("/");
+//            } else {
+//                pathDir.remove(pathDir.size() - 1);
+//            }
+//                return;
+//        }
+
+        if (path == "/") {
             pathDir.removeAll(pathDir);
             pathDir.add("/");
-        } else {
+            return;
+        }
+        if  (!path.startsWith("/")) {
             pathDir.add("/" + path);
+        } else {
+            String[] splittedStr = path.split("(?=/)");
+            pathDir = Arrays.asList(splittedStr);
+        }
+        if (pathDir.get(pathDir.size() - 1).equals("/..")) {
+            pathDir.remove("/..");
+            pathDir.set(pathDir.size() - 1, "/");
         }
     }
 
@@ -21,3 +45,13 @@ public class Shell {
         return pathFull.toString();
     }
 }
+
+//            if (pathDir.size() <= 1) {
+//                pathDir.add(0, "/");
+//                return;
+//            } else {
+//                pathDir.remove(pathDir.size() - 1);
+//                return;
+//            }
+
+//            pathDir.add(path.substring(path.lastIndexOf('/') + 3));
