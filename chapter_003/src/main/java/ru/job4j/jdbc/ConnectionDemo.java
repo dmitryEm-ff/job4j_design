ebe17acabbd27913c7cea8f69d2e2e798d1a397e
BBergsJ <goldlike888@gmail.com>
2021-01-27 23:58:12 +0300

    jdbc

--- chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
+++ chapter_003/src/main/java/ru/job4j/jdbc/ConnectionDemo.java
@@ -1,44 +1,23 @@
 package ru.job4j.jdbc;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.*;
+import ru.job4j.io.Config;
 
 public class ConnectionDemo {
-    private static String url;
-    private static String login;
-    private static String password;
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        Class.forName("org.postgresql.Driver");
-//        String url = "jdbc:postgresql://localhost:5432/idea_db";
-//        String login = "postgres";
-//        String password = "password";
+        Config config = new Config("app.properties");
+        config.load();
 
-        Map<String, String> values = new HashMap<String, String>();
+        String url = config.value("hibernate.connection.url");
+        String login = config.value("hibernate.connection.username");
+        String password = config.value("hibernate.connection.password");
+        String driver = config.value("hibernate.connection.driver_class");
 
-        try (BufferedReader reader = new BufferedReader(new FileReader("app.properties"))) {
-            reader.lines()
-                    .filter(s -> !s.contains("#") || !s.isEmpty())
-                    .map(l -> l.split("="))
-                    .forEach(k -> values.put(k[0], k[1]));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        for (Map.Entry<String, String> keys : values.entrySet()) {
-            if (keys.getKey().contains("url")) {
-                url = keys.getValue();
-            } else if (keys.getKey().contains("username")) {
-                login = keys.getValue();
-            } else if (keys.getKey().contains("password")) {
-                password = keys.getValue();
-            }
-        }
+        Class.forName(driver);
 
         try (Connection connection = DriverManager.getConnection(url, login, password)) {
             DatabaseMetaData metaData = connection.getMetaData();
