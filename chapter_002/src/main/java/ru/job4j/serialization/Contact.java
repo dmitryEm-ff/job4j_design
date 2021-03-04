fe3fcdf2ab3dc653ac1a0b2338cef0fd9a8053c7
BBergsJ <goldlike888@gmail.com>
2021-03-04 22:21:33 +0300

    JAXB. Преобразование XML в POJO

--- chapter_002/src/main/java/ru/job4j/serialization/Contact.java
+++ chapter_002/src/main/java/ru/job4j/serialization/Contact.java
@@ -1,10 +1,18 @@
 package ru.job4j.serialization;
 
+import com.sun.xml.txw2.annotation.XmlElement;
+
+import javax.xml.bind.annotation.XmlAttribute;
 import java.io.*;
 import java.nio.file.Files;
 
+@XmlElement(value = "contact")
 public class Contact {
-    private final String phone;
+    @XmlAttribute
+    private String phone;
+
+    public Contact() {
+    }
 
     public Contact(String phone) {
         this.phone = phone;
--- chapter_002/src/main/java/ru/job4j/serialization/Person.java
+++ chapter_002/src/main/java/ru/job4j/serialization/Person.java
@@ -3,19 +3,33 @@ package ru.job4j.serialization;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.annotation.*;
+import java.io.StringWriter;
 import java.util.Arrays;
 
+@XmlRootElement(name = "person")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Person {
-    private final boolean sex;
-    private final int age;
-    private final Contact contact;
-    private final String[] statuses;
+    @XmlAttribute
+    private boolean sex;
+    @XmlAttribute
+    private int age;
+    private Contact contact;
+    @XmlElementWrapper(name = "statuses")
+    @XmlElement(name = "status")
+    private String[] status;
+
+    public Person() {
+    }
 
     public Person(boolean sex, int age, Contact contact, String... statuses) {
         this.sex = sex;
         this.age = age;
         this.contact = contact;
-        this.statuses = statuses;
+        this.status = statuses;
     }
 
     public boolean isSex() {
@@ -31,7 +45,7 @@ public class Person {
     }
 
     public String[] getStatuses() {
-        return statuses;
+        return status;
     }
 
     @Override
@@ -40,31 +54,47 @@ public class Person {
                 + "sex=" + sex
                 + ", age=" + age
                 + ", contact=" + contact
-                + ", statuses=" + Arrays.toString(statuses)
+                + ", statuses=" + Arrays.toString(status)
                 + '}';
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JAXBException {
+
         final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
 
-        /* Преобразуем объект person в json-строку. */
-        final Gson gson = new GsonBuilder().create();
-        System.out.println(gson.toJson(person));
+        JAXBContext context = JAXBContext.newInstance(Person.class);
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
 
-        /* Модифицируем json-строку */
-        final String personJson =
-                "{"
-                        + "\"sex\":false,"
-                        + "\"age\":35,"
-                        + "\"contact\":"
-                        + "{"
-                        + "\"phone\":\"+7(924)111-111-11-11\""
-                        + "},"
-                        + "\"statuses\":"
-                        + "[\"Student\",\"Free\"]"
-                        + "}";
-        final Person personMod = gson.fromJson(personJson, Person.class);
-        System.out.println(personMod);
+        try (StringWriter writer = new StringWriter()) {
+            marshaller.marshal(person, writer);
+            String result = writer.getBuffer().toString();
+            System.out.println(result);
+        } catch (Exception e) {
+
+        }
     }
 
+//    public static void main(String[] args) {
+//        final Person person = new Person(false, 30, new Contact("11-111"), "Worker", "Married");
+//
+//        /* Преобразуем объект person в json-строку. */
+//        final Gson gson = new GsonBuilder().create();
+//        System.out.println(gson.toJson(person));
+//
+//        /* Модифицируем json-строку */
+//        final String personJson =
+//                "{"
+//                        + "\"sex\":false,"
+//                        + "\"age\":35,"
+//                        + "\"contact\":"
+//                        + "{"
+//                        + "\"phone\":\"+7(924)111-111-11-11\""
+//                        + "},"
+//                        + "\"statuses\":"
+//                        + "[\"Student\",\"Free\"]"
+//                        + "}";
+//        final Person personMod = gson.fromJson(personJson, Person.class);
+//        System.out.println(personMod);
+//    }
 }
