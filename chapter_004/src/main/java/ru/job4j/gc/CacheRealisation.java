5632ac2a11b4f6ea8990c88957501e8059437507
BBergsJ <goldlike888@gmail.com>
2021-03-20 22:56:38 +0300

    Реализации кеша на SoftReference

--- chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
+++ chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
@@ -1,4 +1,10 @@
 package ru.job4j.gc;
 
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CacheRealisation {
+    private final Map<String, Path> cache = new HashMap<>();
+
 }
