6567566a02bb26b5247769b49ab67941f18b8fcc
BBergsJ <goldlike888@gmail.com>
2021-03-22 21:49:18 +0300

    Реализации кеша на SoftReference

--- chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
+++ chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
@@ -1,10 +1,23 @@
 package ru.job4j.gc;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CacheRealisation {
     private final Map<String, Path> cache = new HashMap<>();
 
+    public void readFromCache(String fileName) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("/data/" + fileName))) {
+            if (!cache.containsKey(fileName)) {
+                cache.put(fileName, Paths.get(fileName));
+            }
+
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+    }
 }
