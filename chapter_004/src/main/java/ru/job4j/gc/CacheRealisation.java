80c94d8abf8d96ef2fa9390e26bbb1504cbce1b8
BBergsJ <goldlike888@gmail.com>
2021-03-24 21:05:44 +0300

    Реализации кеша на SoftReference

--- chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
+++ chapter_004/src/main/java/ru/job4j/gc/CacheRealisation.java
@@ -3,14 +3,12 @@ package ru.job4j.gc;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.lang.ref.SoftReference;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class CacheRealisation {
-    private static final Map<String, String> CACHE = new HashMap<>();
+    private static final Map<String, SoftReference<String>> CACHE = new HashMap<>();
 
     public void getCacheValue(String fileName) {
         if (!CACHE.containsKey(fileName)) {
@@ -19,14 +17,14 @@ public class CacheRealisation {
         System.out.println(CACHE.get(fileName));
     }
 
-    public String putCacheValue(String fileName) {
+    public SoftReference<String> putCacheValue(String fileName) {
         String tmp;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader("./chapter_004/data/" + fileName))) {
             tmp = bufferedReader.lines().collect(Collectors.joining(System.lineSeparator()));
         } catch (Exception e) {
             throw new IllegalStateException(e);
         }
-        return tmp;
+        return new SoftReference<String>(tmp);
     }
 
     public static void main(String[] args) {
@@ -34,9 +32,9 @@ public class CacheRealisation {
         cr.getCacheValue("Address.txt");
         System.out.println(System.lineSeparator());
         cr.getCacheValue("Names.txt");
-        for (Map.Entry<String, String> pair : CACHE.entrySet()) {
+        for (Map.Entry<String, SoftReference<String>> pair : CACHE.entrySet()) {
             String key = pair.getKey();
-            String value = pair.getValue();
+            SoftReference<String> value = pair.getValue();
             System.out.println(System.lineSeparator() + key + " : " + System.lineSeparator() + value);
         }
     }
