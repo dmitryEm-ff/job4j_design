40573c2576f7802745a42938bd1d554503119b64
BBergsJ <goldlike888@gmail.com>
2021-04-05 22:05:56 +0300

    GC

---
+++ chapter_004/src/main/java/ru/job4j/jmh/Runner.java
@@ -0,0 +1,10 @@
+package ru.job4j.jmh;
+
+import java.io.IOException;
+
+public class Runner {
+
+    public static void main(String[] args) throws IOException {
+        org.openjdk.jmh.Main.main(args);
+    }
+}
