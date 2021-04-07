788d7063b3f22344d288d86a431f5fe6699f7828
BBergsJ <goldlike888@gmail.com>
2021-04-07 23:58:34 +0300

    Принципы Kiss, Dry и Yagni

--- chapter_005/src/main/java/ru/job4j/kiss/MaxMin.java
+++ chapter_005/src/main/java/ru/job4j/kiss/MaxMin.java
@@ -1,27 +1,35 @@
 package ru.job4j.kiss;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.BiPredicate;
 
 public class MaxMin {
 
-    public <T> T max(List<T> value, Comparator<T> comparator) {
-        T maximum = value.get(0);
-        for (T t : value) {
-            if (comparator.compare(maximum, t) < 0) {
-                maximum = t;
-            }
-        }
-        return maximum;
+    public static <T> T max(List<T> value, Comparator<T> comparator) {
+        BiPredicate<T, T> maxPredic = (t, u) -> (comparator.compare(t, u) < 0);
+        return manualComparator(value, maxPredic);
     }
 
-    public <T> T min(List<T> value, Comparator<T> comparator) {
-        T minimum = value.get(0);
-        for (T t : value) {
-            if (comparator.reversed().compare(minimum, t) < 0) {
-                minimum = t;
+    public static <T> T min(List<T> value, Comparator<T> comparator) {
+        BiPredicate<T, T> minPredic = (t, u) -> (comparator.reversed().compare(t, u) < 0);
+        return manualComparator(value, minPredic);
+    }
+
+    private static <T> T manualComparator(List<T> list, BiPredicate<T, T> predicate) {
+        T valuePoint = list.get(0);
+        for (T t : list) {
+            if (predicate.test(valuePoint, t)) {
+                valuePoint = t;
             }
         }
-        return minimum;
+        return valuePoint;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> testList = Arrays.asList(5, 15, 100, -6, 11, 20);
+        System.out.println(max(testList, Integer::compareTo));
+        System.out.println(min(testList, Integer::compareTo));
     }
 }
