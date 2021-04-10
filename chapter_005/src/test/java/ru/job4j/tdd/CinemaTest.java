8686075bf270edf980f937e3e0480c0263d8c9e0
BBergsJ <goldlike888@gmail.com>
2021-04-10 14:06:19 +0300

    TDD

--- chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
+++ chapter_005/src/test/java/ru/job4j/tdd/CinemaTest.java
@@ -36,4 +36,54 @@ public class CinemaTest {
         List<Session> sessions = cinema.find(s -> true);
         assertThat(true, is(sessions.get(0)));
     }
+
+    @Test
+    public void whenSameSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 04, 10, 15, 00);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+
+        Account secondAccount = new AccountCinema();
+        Calendar secondDate = Calendar.getInstance();
+        secondDate.set(2021, 04, 10, 15, 30);
+        Ticket secondTicket = cinema.buy(secondAccount, 1, 1, secondDate);
+
+        Ticket result = null;
+        assertThat(secondTicket, is(result));
+    }
+
+    @Test
+    public void whenWrongDate() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2022, 05, 10, 15, 30);
+        Ticket ticket = cinema.buy(account, 1, 1, date);
+
+        Ticket result = null;
+        assertThat(ticket, is(result));
+    }
+
+    @Test
+    public void whenWrongSeat() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema3D();
+        Calendar date = Calendar.getInstance();
+        date.set(2021, 04, 10, 15, 00);
+        Ticket ticket = cinema.buy(account, 99, 99, date);
+
+        Ticket result = null;
+        assertThat(ticket, is(result));
+    }
+
+    @Test
+    public void whenWrongCinema() {
+        Account account = new AccountCinema();
+        Cinema cinema = new Cinema4D();
+
+        List<Session> sessions = cinema.find(session -> true);
+        assertThat(sessions.size(), is(0));
+    }
 }
