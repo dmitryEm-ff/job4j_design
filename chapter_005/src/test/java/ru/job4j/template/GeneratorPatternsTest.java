b844667ef0a8ab72d0fa52034fa0c41e206513aa
BBergsJ <goldlike888@gmail.com>
2021-04-10 14:40:50 +0300

    Шаблонизатор

--- chapter_005/src/test/java/ru/job4j/template/GeneratorPatternsTest.java
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorPatternsTest.java
@@ -11,23 +11,71 @@ import static org.hamcrest.Matchers.is;
 
 public class GeneratorPatternsTest {
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenNotContains() {
+    @Test
+    public void whenCorrectTemplates() {
+        String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> args = new HashMap<>();
-        args.put("name", "Igor");
+        args.put("name", "Petr Arsentev");
         args.put("subject", "you");
-
-        String template = "Petr Arsentev";
+        String expected = "I am a Petr Arsentev, Who are you? ";
         GeneratorPatterns testGen = new GeneratorPatterns();
-        testGen.produce(template, args);
+        String out = testGen.produce(template, args);
+        assertThat(out, is(expected));
     }
 
-    @Test(expected = IllegalArgumentException.class)
-    public void whenExcess() {
+    @Test
+    public void whenTemplateHaveDiffArgs() {
+        String template = "I am a ${person}, Who are ${subject}? ";
         Map<String, String> args = new HashMap<>();
-        args.put("name", "Igor");
+        args.put("name", "Petr Arsentev");
         args.put("subject", "you");
-
-        args.get("year");
+        boolean result = false;
+        try {
+            String out = new GeneratorPatterns().produce(template, args);
+        } catch (Exception e) {
+            result = true;
+        } finally {
+            assertThat(result, is(true));
+        }
     }
+
+    @Test
+    public void whenHaveMoreArgs() {
+        String template = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        args.put("profession", "worker");
+        boolean result = false;
+        try {
+            String out = new GeneratorPatterns().produce(template, args);
+        } catch (Exception e) {
+            result = true;
+        } finally {
+            assertThat(result, is(true));
+        }
+    }
+
+
+
+
+//    @Test(expected = IllegalArgumentException.class)
+//    public void whenNotContains() {
+//        Map<String, String> args = new HashMap<>();
+//        args.put("name", "Igor");
+//        args.put("subject", "you");
+//
+//        String template = "Petr Arsentev";
+//        GeneratorPatterns testGen = new GeneratorPatterns();
+//        testGen.produce(template, args);
+//    }
+//
+//    @Test(expected = IllegalArgumentException.class)
+//    public void whenExcess() {
+//        Map<String, String> args = new HashMap<>();
+//        args.put("name", "Igor");
+//        args.put("subject", "you");
+//
+//        args.get("year");
+//    }
 }
