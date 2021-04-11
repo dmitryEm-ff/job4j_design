b2ea3cde2b8151eb8be2c2bf47631d3c4388219a
BBergsJ <goldlike888@gmail.com>
2021-04-11 17:29:03 +0300

    Шаблонизатор

--- chapter_005/src/test/java/ru/job4j/template/GeneratorPatternsTest.java
+++ chapter_005/src/test/java/ru/job4j/template/GeneratorPatternsTest.java
@@ -23,59 +23,22 @@ public class GeneratorPatternsTest {
         assertThat(out, is(expected));
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void whenTemplateHaveDiffArgs() {
         String template = "I am a ${person}, Who are ${subject}? ";
         Map<String, String> args = new HashMap<>();
         args.put("name", "Petr Arsentev");
         args.put("subject", "you");
-        boolean result = false;
-        try {
-            String out = new GeneratorPatterns().produce(template, args);
-        } catch (Exception e) {
-            result = true;
-        } finally {
-            assertThat(result, is(true));
-        }
+        String out = new GeneratorPatterns().produce(template, args);
     }
 
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void whenHaveMoreArgs() {
         String template = "I am a ${name}, Who are ${subject}? ";
         Map<String, String> args = new HashMap<>();
         args.put("name", "Petr Arsentev");
         args.put("subject", "you");
         args.put("profession", "worker");
-        boolean result = false;
-        try {
-            String out = new GeneratorPatterns().produce(template, args);
-        } catch (Exception e) {
-            result = true;
-        } finally {
-            assertThat(result, is(true));
-        }
+        String out = new GeneratorPatterns().produce(template, args);
     }
-
-
-
-
-//    @Test(expected = IllegalArgumentException.class)
-//    public void whenNotContains() {
-//        Map<String, String> args = new HashMap<>();
-//        args.put("name", "Igor");
-//        args.put("subject", "you");
-//
-//        String template = "Petr Arsentev";
-//        GeneratorPatterns testGen = new GeneratorPatterns();
-//        testGen.produce(template, args);
-//    }
-//
-//    @Test(expected = IllegalArgumentException.class)
-//    public void whenExcess() {
-//        Map<String, String> args = new HashMap<>();
-//        args.put("name", "Igor");
-//        args.put("subject", "you");
-//
-//        args.get("year");
-//    }
 }
