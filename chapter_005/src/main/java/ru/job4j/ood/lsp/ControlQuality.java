d505e8da4d5a2eb6fef0c14faaf67bc4c1a80d08
BBergsJ <goldlike888@gmail.com>
2021-04-17 23:41:56 +0300

    Хранилище продуктов

--- chapter_005/src/main/java/ru/job4j/ood/lsp/ControlQuality.java
+++ chapter_005/src/main/java/ru/job4j/ood/lsp/ControlQuality.java
@@ -8,43 +8,29 @@ public class ControlQuality implements Control {
 
     @Override
     public Storage sort(Food food) {
-//        int point = (createdToExp / 100) * 25;
-//        if (createdToCurrent < point) {
-//            Storage storage = new Warehouse();
-//            storage.addFood(food);
-//        }
         Storage storage = findStorage(food);
         storage.addFood(food);
         return storage;
     }
 
     public Storage findStorage(Food food) {
-        int createdToExp = (int) DAYS.between(food.getCreateDate(), food.getExpiryDate());
-        int createdToCurrent = (int) DAYS.between(food.getCreateDate(), LocalDateTime.now());
-
         if (isTrash(food)) {
             return new Trash();
         }
-        if (isWarehouse(food, createdToExp, createdToCurrent)) {
+        if (isWarehouse(food)) {
             return new Warehouse();
-        }
-        if (isShopWithDiscount(food, createdToExp, createdToCurrent)) {
-            return new Shop();
         }
         return new Shop();
     }
 
-    public boolean isWarehouse(Food food, int createdToExp, int createdToCurrent) {
-        int point = (createdToExp / 100) * 25;
-        return createdToCurrent < point;
-    }
-
-    public boolean isShopWithDiscount(Food food, int createdToExp, int createdToCurrent) {
-        return false;
-
-    }
-
     public boolean isTrash(Food food) {
         return DAYS.between(LocalDateTime.now(), food.getExpiryDate()) < 0;
+    }
+
+    public boolean isWarehouse(Food food) {
+        int full = (int) DAYS.between(food.getCreateDate(), food.getExpiryDate());
+        int current = (int) DAYS.between(food.getCreateDate(), LocalDateTime.now());
+        int point = (full / 100) * 25;
+        return current < point;
     }
 }
--- chapter_005/src/main/java/ru/job4j/ood/lsp/Shop.java
+++ chapter_005/src/main/java/ru/job4j/ood/lsp/Shop.java
@@ -1,13 +1,22 @@
 package ru.job4j.ood.lsp;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 public class Shop implements Storage {
     private final List<Food> store = new ArrayList<>();
 
     @Override
     public void addFood(Food food) {
+        int full = (int) DAYS.between(food.getCreateDate(), food.getExpiryDate());
+        int current = (int) DAYS.between(food.getCreateDate(), LocalDateTime.now());
+        int point = (full / 100) * 75;
+        if (current > point) {
+            food.setDiscount(30);
+        }
         store.add(food);
     }
 }
