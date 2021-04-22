05eebcba2c62b4701badce16c381338770380464
BBergsJ <goldlike888@gmail.com>
2021-04-23 00:12:16 +0300

    Парковка машин. Тесты

--- chapter_005/src/main/java/ru/job4j/ood/parking/NewParking.java
+++ chapter_005/src/main/java/ru/job4j/ood/parking/NewParking.java
@@ -2,6 +2,7 @@ package ru.job4j.ood.parking;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class NewParking implements Parking {
 
@@ -56,7 +57,25 @@ public class NewParking implements Parking {
 
     public static void main(String[] args) {
         Parking parking = new NewParking(10, 5);
-        System.out.println(parking.getAllBigCars().size());
         System.out.println(parking.getAllSmallCars().size());
+        System.out.println(parking.getAllBigCars().size());
+
+        Car passengerCar = new PassengerCar();
+        Car cargoCar = new CargoCar(3);
+
+        parking.getAllSmallCars().set(0, new Place(1, true, passengerCar));
+        parking.getAllBigCars().set(0, new Place(1, true, cargoCar));
+        parking.getAllSmallCars().set(1, new Place(2, true, cargoCar));
+
+
+        for (Place p : parking.getAllSmallCars()) {
+            Optional<Car> test1 = Optional.ofNullable(p.getCar());
+            System.out.println(test1.orElse(null));
+        }
+
+        for (Place p : parking.getAllBigCars()) {
+            Optional<Car> test2 = Optional.ofNullable(p.getCar());
+            System.out.println(test2.orElse(null));
+        }
     }
 }
