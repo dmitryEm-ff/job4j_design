1b90bad0eb88887ac71ef583f77e476d1bcebe08
BBergsJ <goldlike888@gmail.com>
2021-05-03 19:46:06 +0300

    Создать меню

--- chapter_005/src/main/java/ru/job4j/ood/isp/MainMenu.java
+++ chapter_005/src/main/java/ru/job4j/ood/isp/MainMenu.java
@@ -1,10 +1,12 @@
 package ru.job4j.ood.isp;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class MainMenu implements Menu {
     private String title;
     private List<MenuItem> menuItems;
+    private Scanner scanner = new Scanner(System.in);
 
     public MainMenu(String title, List<MenuItem> menuItems) {
         this.title = title;
@@ -12,7 +14,22 @@ public class MainMenu implements Menu {
     }
 
     @Override
-    public void showMenu(List<MenuItem> menuItems) {
+    public void init(List<MenuItem> menuItems) {
+        boolean run = true;
+        while (run) {
+            showMenu(menuItems);
+            System.out.println("Select something: ");
+            int select = Integer.valueOf(scanner.nextLine());
+            MenuItem item = menuItems.get(select);
+            run = item.getUserAction().execute();
+        }
+    }
 
+    @Override
+    public void showMenu(List<MenuItem> menuItems) {
+        System.out.println(title);
+        for (MenuItem menuItem : menuItems) {
+            System.out.println(menuItem.getTitle());
+        }
     }
 }
--- chapter_005/src/main/java/ru/job4j/ood/isp/Menu.java
+++ chapter_005/src/main/java/ru/job4j/ood/isp/Menu.java
@@ -3,5 +3,6 @@ package ru.job4j.ood.isp;
 import java.util.List;
 
 public interface Menu {
+    void init(List<MenuItem> menuItems);
     void showMenu(List<MenuItem> menuItems);
 }
