609f38b13b0262782de17c268f42b2955dcdb1b1
BBergsJ <goldlike888@gmail.com>
2021-11-16 21:40:38 +0300

    HQL [#6874]

---
+++ chapter_005/src/main/java/ru/job4j/hbm/hql/Candidate.java
@@ -0,0 +1,83 @@
+package ru.job4j.hbm.hql;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name = "candidates")
+public class Candidate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    private int exp;
+
+    private int salary;
+
+    public static Candidate of(String name, int exp, int salary) {
+        Candidate candidate = new Candidate();
+        candidate.name = name;
+        candidate.exp = exp;
+        candidate.salary = salary;
+        return candidate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Candidate candidate = (Candidate) o;
+        return id == candidate.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Candidate: id=%s, name=%s, exp=%s, salary=%s",
+                id, name, exp, salary);
+    }
+}
---
+++ chapter_005/src/main/java/ru/job4j/hbm/hql/CandidateMain.java
@@ -0,0 +1,67 @@
+package ru.job4j.hbm.hql;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import java.util.List;
+
+public class CandidateMain {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry).buildMetadata().buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+
+            Candidate one = Candidate.of("one", 2, 10000);
+            Candidate two = Candidate.of("two", 10, 50000);
+            Candidate three = Candidate.of("three", 5, 30000);
+
+            session.save(one);
+            session.save(two);
+            session.save(three);
+
+            //Выбрать всех Candidate
+            List<Candidate> candidateList = session.createQuery("from Candidate").list();
+
+            //Выбрать через setParameter
+            Candidate candidateTestOne = (Candidate) session.createQuery("from Candidate c where c.id = :id")
+                    .setParameter("id", 2).uniqueResult();
+
+            //Выбрать по имени
+            Candidate candidateTestTwo = (Candidate) session.createQuery("from Candidate c where c.name = 'three'")
+                    .uniqueResult();
+
+            //Обновить кандидата
+            session.createQuery("update Candidate c set c.name = :name, c.exp = :exp, c.salary = :salary where c.id = :id")
+                    .setParameter("name", "changed")
+                    .setParameter("exp", 100)
+                    .setParameter("salary", 500000)
+                    .setParameter("id", 1)
+                    .executeUpdate();
+
+            //Удалить по id
+            session.createQuery("delete from Candidate where id = :id")
+                    .setParameter("id", 2)
+                    .executeUpdate();
+
+            //Добавить новую запись через concat()
+            session.createQuery("insert into Candidate (name, exp, salary) "
+            + "select concat(c.name, 'NEW'), c.exp + 5, c.salary + 1000000 "
+            + "from Candidate c where c.id = :id")
+                    .setParameter("id", 1)
+                    .executeUpdate();
+
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
